e63256cb5148ae29d1cba23624f81fef5181834e
codingyou <codingyou@naver.com>
2021-06-16 01:23:38 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,14 +3,14 @@ questions = [
         "question": "현대 자본주의는 불평등을 야기하는 직접적인 원인이다.",
         "effect": {
             "score": 10,
-            "answer": 1,
+            "answer": 1
         }
     },
 	   {
         "question": "국민의힘과 같은 보수정당은 그 역할을 제대로 수행하고 있다.",
         "effect": {
             "score": 10,
-            "answer": 2,
+            "answer": 2
         }
     }
 ];
